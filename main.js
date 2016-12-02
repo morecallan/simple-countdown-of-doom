d8cd4d487e729c4d44bd85bb2c24a7d77eea1c51
Callan Morrison <callan.l.morrison@gmail.com>
2016-12-01 23:25:37 -0600

    raw functionality and display

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const before_enter = document.getElementById("before_enter")
+const after_enter = document.getElementById("after_enter")
+const scott_said = document.getElementById("scott_said")
+
+
 function getTimeRemaining(endtime){
   var end_time_date = new Date()
   var hours = endtime.split(":")[0]
@@ -6,35 +11,48 @@ function getTimeRemaining(endtime){
   end_time_date.setMinutes(minutes)
   end_time_date.setSeconds(0)
   var t = Date.parse(end_time_date) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
+  if (t < 0) {
+    return 'Silly Scott. That time already happened.'
+  } else {
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    }
+  }
 }
 
 function initializeClock(id, endtime){
   var clock = document.getElementById(id);
   var timeinterval = setInterval(function(){
     var t = getTimeRemaining(endtime);
-    clock.innerHTML = 'days: ' + t.days + '<br>' +
-                      'hours: '+ t.hours + '<br>' +
-                      'minutes: ' + t.minutes + '<br>' +
-                      'seconds: ' + t.seconds;
-    if(t.total<=0){
+    if (typeof t == "object") {
+      let endTimeHours = Number(endtime.split(':')[0]) > 12 ? Number(endtime.split(':')[0]) - 12 : endtime.split(':')[0]
+      let displayTime = endTimeHours + ':' + endtime.split(':')[1]
+      scott_said.innerHTML = `Scott said be back at ${displayTime}.`
+      clock.innerHTML = '<div> The learning commences in... </div>' +
+                        `<div> ${t.minutes} minutes </div>` +
+                        `<div> ${t.seconds} seconds </div>`
+      if(t.total<=0){
+        clearInterval(timeinterval);
+      }
+    } else {
+      scott_said.innerHTML = t;
       clearInterval(timeinterval);
     }
+
   },1000);
 }
 
 function setDoomsDay() {
   var time_set = document.getElementById("input_time").value
-  console.log(time_set)
+  before_enter.classList.add("hidden")
+  after_enter.classList.remove("hidden")
   initializeClock('clockdiv', time_set);
 }
